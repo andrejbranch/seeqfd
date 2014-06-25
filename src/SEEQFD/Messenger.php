5703944d8009694830e815f0081ed5a59e0c4579
andrejbranch <andrejbranch@gmail.com>
2014-06-25 13:28:02 -0500

    some updates using symfony console command

--- src/SEEQFD/Messenger.php
+++ src/SEEQFD/Messenger.php
@@ -2,17 +2,16 @@
 
 namespace SEEQFD;
 
-use React\ZMQ\Context;
 use Ratchet\ConnectionInterface;
 
 class Messenger
 {
     public function send(ConnectionInterface $from, $message)
     {
-        $context = new ZMQContext();
-        $socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
-        $socket->connect("tcp://localhost:5555")
+        $context = new \ZMQContext();
+        $socket = $context->getSocket(\ZMQ::SOCKET_PUSH, 'my pusher');
+        $socket->connect("tcp://localhost:5555");
 
-        $socket->send('test');
+        $socket->send(json_encode(array('test')));
     }
 }
